c5c5c02729031732a1b4fdbdc359de6b4a32bbd7
Suprio-Das <priyamd507.gmail.com>
2024-10-02 23:14:19 +0600

    Navbar styled

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <Link to={'/'}>Home</Link>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/countries">Countries</NavLink>
+        <nav className="navbar">
+            <ul className="navbar-list">
+                <li className='text-primary'><Link to="/" className="nav-link">Home</Link></li>
+                <li><NavLink to="/about" className="nav-link">About</NavLink></li>
+                <li><NavLink to="/countries" className="nav-link">Countries</NavLink></li>
             </ul>
         </nav>
     );
